fab85bf41999415370c03879f1c6e71aac3d2018
HallsRetail <100135421+HallsRetail@users.noreply.github.com>
2022-05-12 15:38:48 +0200

    Update: Authentication
    Updated the authentication views and layout files.

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.app')
-
+@extends('layouts.auth')
+@section('title', 'Verify Your Email Address')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
